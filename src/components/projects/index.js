b0f52dd47ae1d5dabc8f2a9963e4e5a5c94fcac1
Rubanza Silver <rubanza@rubanzasilver.xyz>
2023-05-20 14:58:05 +0300

    adding project section skeleton changes

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Card } from 'antd';
+import {GridWrapper} from './projectElements'
 
 const { Meta } = Card;
 
 const Projects: React.FC = () => (
-  <Card
+  <GridWrapper>
+    <Card
     hoverable
     style={{ width: 240 }}
     cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
   >
     <Meta title="Europe Street beat" description="www.instagram.com" />
   </Card>
+  </GridWrapper>
 );
 
 export default Projects;
---
+++ src/components/projects/projectElements.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+export const GridWrapper = styled.ul`
+ display:grid;
+ grid-template-columns:1fr 1fr;
+ background-color:#fff;
+ list-style-type:none;
+ height:900px;
+ grid-gap:16px;
+ `
