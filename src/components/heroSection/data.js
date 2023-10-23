9afb978df99e7ba50c88ea63045155c0eeccf4db
Rubanza Silver <rubanza@rubanzasilver.xyz>
2023-10-23 13:30:09 +0300

    adding new cv
    - changes in navbar
    -changes in herosection content

--- src/components/heroSection/data.js
+++ src/components/heroSection/data.js
@@ -2,7 +2,7 @@
 export const SectionAinfo = {
     id:'SelfIntro',
     TitleText:'Hi, I am Rubanza Silver',
-    headline:'A Data Scientist with a background in Software Enginnering',
+    headline:'A coder with a background in Software Enginnering',
     text:'My work and interest lies in working on various steps of the machine learning lifecycle from EDA, Data wrangling, Feature Engineering to Model building , deployment , testing, monitoring etc',
     text1:'I also have vast experience building web applications with javascript, reactjs etc.',
     text2:'Below are examples of my work',
